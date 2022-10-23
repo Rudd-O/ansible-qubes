45cd87d984b0f761a4a4ba2a11cde7ee9dcf3fe2
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2022-10-23 02:47:08 -0400

    Update qubesformation.py

--- action_plugins/qubesformation.py
+++ action_plugins/qubesformation.py
@@ -21,11 +21,8 @@ def generate_datastructure(vms, task_vars):
         # This block will skip any VMs that are not in the groups defined in the 'formation_vm_groups' variable
         # This allows you to deploy in multiple stages which is useful in cases
         # where you want to create a template after another template is already provisioned.
-        print(task_vars.ansible_facts)
         print("hostvars")
         print(task_vars['hostvars'][n])
-        print("formation")
-        print(task_vars.ansible_facts['formation_vm_groups'])
         print("testy")
         print(task_vars['hostvars'][n]['formation_vm_groups'])
         if hasattr(task_vars.ansible_facts, 'formation_vm_groups'):
